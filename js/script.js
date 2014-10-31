82ab490b0cda139c7c72d65bb82a2b535426f391
Bruno Oliveira <bruno.oliveirati1@gmail.com>
2014-10-31 01:13:31 -0200

    Variavel Discreta/Simples

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ function binomialDistribution() {
     var r = parseInt($('#r').val());
     var p = $("#p").val().split(",");
 
-    if(p.length === 2){
-    	p = parseFloat(p[0]+"."+p[1])
+    if (p.length === 2) {
+        p = parseFloat(p[0] + "." + p[1])
     }
 
     if (p < 0 || p > 1) {
@@ -32,16 +32,82 @@ function binomialDistribution() {
     var ncr = npr / rf;
     var des = ncr * Math.pow(p, r) * Math.pow((1 - p), (n - r));
     $("#result-decimal").val(Math.round(des * 10000) / 10000);
-    $("#result-percent").val((Math.round(des * 10000) / 10000)*100);
+    $("#result-percent").val((Math.round(des * 10000) / 10000) * 100);
 }
 
+var dataTable;
+var dvchart;
+function variableDistribution() {
+    var numbersArray = $('input[name=dvnumbers]').val().split(';');
+    var frequencyAndNumbers = new Array;
+    var cumulativeFrequency = 0;
+    var totalFrequency = 0, relativeFrequency = new Array;
+    // [N,Fa,Fr,Fra]
+    for (var i = 0; i < numbersArray.length; i++) {
+        frequencyAndNumbers[i] = numbersArray[i].split('-');
+    }
+    for (var i = 0; i < frequencyAndNumbers.length; i++) {
+        totalFrequency = totalFrequency + parseFloat(frequencyAndNumbers[i][0]);
+    }
+    console.log(totalFrequency);
+    for (var i = 0; i < frequencyAndNumbers.length; i++) {
+        relativeFrequency[i] = Math.round(((100 * parseFloat(frequencyAndNumbers[i][0]) / totalFrequency) * 100) / 100);
+    }
+    var finalData = new Array;
+    var chartData = new Array;
+    var chartLabel = new Array;
+    for (var i = 0; i < frequencyAndNumbers.length; i++) {
+        cumulativeFrequency = cumulativeFrequency + parseFloat(relativeFrequency[i]);
+        finalData[i] = [frequencyAndNumbers[i][0], frequencyAndNumbers[i][1], relativeFrequency[i], cumulativeFrequency];
+        chartData[i] = [relativeFrequency[i]];
+        chartLabel[i] = {label:frequencyAndNumbers[i][1]};
+    }
+    $('#dvtable .title').contents().remove();
+    $('#dvtable .title').append('N of ' + $('input[name=dvname]').val());
+    $('#dvtable').slideDown(1000);
+    dataTable = $('#dvtable').DataTable({
+        paging: false,
+        searching: false,
+        data: finalData
+    });
+    // Can specify a custom tick Array.
+    // Ticks should match up one for each y value (category) in the series.
+    var ticks = [$('input[name=dvname]').val()];
+    dvchart = $.jqplot('dvchart', chartData, {
+        series: chartLabel,
+        seriesColors: ['#45A0B2', '#ACD167', '#FE92CE','#F3BB1C','#EB3D1A'],
+        seriesDefaults: {
+            renderer: $.jqplot.BarRenderer,
+            rendererOptions: {fillToZero: true, varyBarColor: false}
+        },
+        legend: {
+            show: true,
+            placement: 'outsideGrid'
+        },
+        axes: {
+            xaxis: {
+                renderer: $.jqplot.CategoryAxisRenderer,
+                ticks: ticks
+            },
+            yaxis: {
+                pad: 1.05,
+            }
+        }
+    });
+}
 
+$(document).ready(function () {
+    smoothScroll.init();
 
-$(document).ready(function() {
-	smoothScroll.init();
-
-	$('#binomialBtn').click(function(){
-		binomialDistribution();
-	});
+    $('#binomialBtn').click(function () {
+        binomialDistribution();
+    });
+    $('#ddBtn').click(function () {
+        if ($.fn.dataTable.isDataTable('#dvtable')) {
+            dvchart.destroy();
+            dataTable.destroy();
+        }
+        variableDistribution();
+    });
 
 });
